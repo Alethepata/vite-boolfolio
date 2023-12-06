c12784c12cb28e90dffcb813b36a4ec9559b74ca
Alethepata <alessia.patane@gmail.com>
2023-12-06 16:46:09 +0100

    [Add] card

---
+++ src/components/Main.vue
@@ -0,0 +1,33 @@
+<script>
+import { store } from '../data/store';
+
+  export default {
+    name: 'Main',
+    data() {
+      return {
+        store
+      }
+    },
+    computed: {},
+    mounted() {},
+    methods: {}
+  }
+</script>
+
+<template>
+
+  <div class="container">
+    <div class="card">
+      <div class="card-body">
+        <ul>
+          <li v-for="project in store.projects" :key="project.id">{{ project.title }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style>
+
+</style>
