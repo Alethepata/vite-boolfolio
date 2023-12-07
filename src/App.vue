c294d7d107cfc36e8f7f857e4efb2d192e695ed4
Alethepata <alessia.patane@gmail.com>
2023-12-07 15:35:33 +0100

    [delete] axios

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,11 @@
 <script>
-import axios from 'axios';
-import { store } from './data/store';
-
 export default {
-  name: 'App',
-  data() {
-    return {
-      store
-    }
-  },
-  methods: {
-    getApi() {
-      axios.get(store.apiUrl)
-        .then(res =>{
-          store.projects = res.data.projects;
-        })
+  name: 'App'
 
-    }
-
-  },
-  mounted() {
-    this.getApi();
-
-  }
 }
 </script>
 
 <template>
-
   <RouterView />
 </template>
 
