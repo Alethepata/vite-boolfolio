b58b37f3f6de209396091e460149f0e7c20d3b63
Alethepata <alessia.patane@gmail.com>
2023-12-07 17:12:01 +0100

    [add] Header

---
+++ src/components/partials/Header.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    name:'Header'
+}
+</script>
+
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <router-link :to="{name: 'Home'}">Home</router-link>
+                </li>
+                <li>
+                    <router-link :to="{name: 'Projects'}">Projects</router-link>
+                </li>
+                <li>
+                    <router-link :to="{name: 'About'}" >About</router-link>
+                </li>
+                <li>
+                    <router-link :to="{name: 'Contacts'}">Contacts</router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss">
+header{
+    ul{
+        display: flex;
+        list-style: none;
+        li{
+        margin: 10px;
+        }
+    }
+}
+</style>
