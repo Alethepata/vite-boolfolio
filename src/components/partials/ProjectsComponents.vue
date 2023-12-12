a0f5a33d61029064f11366bf16fa7d2f630b6d5c
Alethepata <alessia.patane@gmail.com>
2023-12-12 18:01:56 +0100

    [add] ProjectsComponents

---
+++ src/components/partials/ProjectsComponents.vue
@@ -0,0 +1,51 @@
+<script>
+import { store } from '../../data/store';
+
+export default {
+  name: 'ProjectsComponents',
+  data() {
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="card">        
+      <h1>Projects</h1>
+
+      <div class="card-body">
+        <ul>
+          <li v-for="project in store.projects" :key="project.id">
+            <router-link :to="{name: 'ProjectDetails', params:{slug: project.slug}}">{{ project.title }}</router-link>
+            <em class="type">{{ project.type.name }}</em>
+            <em class="tecnologies" v-for="tecnology in project.tecnologies" :key="tecnology.id">{{ tecnology.name }}</em>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+h1{
+  text-align: center;
+}
+li{
+  display: flex;
+  
+  p{
+    padding: 10px;
+  }
+  .type{
+      font-weight: bold;
+      margin: 0 10px;
+  }
+  .tecnologies{
+    font-style: italic;
+    margin-right:10px;
+  }
+}
+
+</style>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -3,11 +3,14 @@ import axios from 'axios';
 import { store } from '../data/store';
 
 import Loader from '../components/partials/Loader.vue';
+import ProjectsComponents from '../components/partials/ProjectsComponents.vue';
 
 export default {
     name: 'Projects',
     components: {
-        Loader
+        Loader,
+        ProjectsComponents
+  
   },
   data() {
     return {
@@ -38,41 +41,11 @@ mounted() {
     </div>
 
   <div v-else class="container">
-    <div class="card">        
-      <h1>Projects</h1>
-
-      <div class="card-body">
-        <ul>
-          <li v-for="project in store.projects" :key="project.id">
-            <router-link :to="{name: 'ProjectDetails', params:{slug: project.slug}}">{{ project.title }}</router-link>
-            <em class="type">{{ project.type.name }}</em>
-            <em class="tecnologies" v-for="tecnology in project.tecnologies" :key="tecnology.id">{{ tecnology.name }}</em>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <ProjectsComponents />
   </div>
 
 </template>
 
 <style lang="scss" scoped>
-h1{
-  text-align: center;
-}
-li{
-  display: flex;
-  
-  p{
-    padding: 10px;
-  }
-  .type{
-      font-weight: bold;
-      margin: 0 10px;
-  }
-  .tecnologies{
-    font-style: italic;
-    margin-right:10px;
-  }
-}
 
 </style>
