ea57ad85ebb0f3ab56531de33d8c60b2c8846636
Alethepata <alessia.patane@gmail.com>
2023-12-12 17:17:18 +0100

    [add] error 404

---
+++ src/pages/Error404.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'Error404'
+}
+</script>
+
+<template>
+  <div>
+    <p>Error404</p>
+  </div>
+</template>
+
+<style scoped>
+p{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    font-size: 100px;
+    color: red;
+
+}
+
+</style>
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Projects from './pages/Projects.vue';
 import About from './pages/About.vue';
 import Contacts from './pages/Contacts.vue';
 import ProjectDetails from './pages/ProjectDetails.vue';
+import Error404 from './pages/Error404.vue';
 
 const router = createRouter({
     history: createWebHashHistory(),
@@ -33,6 +34,11 @@ const router = createRouter({
             path: '/details/:slug',
             name: 'ProjectDetails',
             component: ProjectDetails,
+        },
+        {
+            path: '/:patchMatch(.*)*',
+            name: 'error-404',
+            component: Error404
         },
     ],
 })
